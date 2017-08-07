a86479d3516c2d4915ca4f78a9ee3d5959ac9b30
ZHUOYI WANG <sduwzy1992@gmail.com>
2017-08-07 10:55:03 -0500

    Update KmeansBeta.py

--- kmeans_lib/KmeansBeta.py
+++ kmeans_lib/KmeansBeta.py
@@ -6,14 +6,17 @@ import math
 import scipy.io as sio
 from kmeans import *
 import numpy as np
+from sklearn.cluster import KMeans
 from kmeans_plus_plus import *
+from Novel_Class.clusteringMethod import *
+from sklearn import svm
 
 
-def readStreamData(dataName,rate,index):
-    sourcePath = 'C:/Matlab_Code/infometric_0.1/Data/' + dataName + '/' + dataName + '_' + str(index) + '_'+str(rate)+'_source_stream.txt'
-    targetPath = 'C:/Matlab_Code/infometric_0.1/Data/' + dataName + '/' + dataName + '_' + str(index) + '_'+str(rate) +'_target_stream.txt'
-    #sourcePath = '/home/wzy/Coding/Data/' + dataName + '/' + dataName + '_' + str(index) + '_'+str(rate) + '_source_stream.txt';
-    #targetPath = '/home/wzy/Coding/Data/' + dataName + '/' + dataName + '_' + str(index) + '_'+str(rate) + '_target_stream.txt';
+def readBetaStreamData(dataName,rate,index):
+    #sourcePath = 'C:/Matlab_Code/infometric_0.1/Data/' + dataName + '/' + dataName + '_' + str(index) + '_'+str(rate)+'_source_stream.txt'
+    #targetPath = 'C:/Matlab_Code/infometric_0.1/Data/' + dataName + '/' + dataName + '_' + str(index) + '_'+str(rate) +'_target_stream.txt'
+    sourcePath = '/home/wzy/Coding/Data/' + dataName + '/' + dataName + '_' + str(index) + '_'+str(rate) + '_source_stream.txt';
+    targetPath = '/home/wzy/Coding/Data/' + dataName + '/' + dataName + '_' + str(index) + '_'+str(rate) + '_target_stream.txt';
 
     sourceFeature = []
     targetFeature = []
@@ -52,18 +55,6 @@ def getNovelClass(tranTargetFeature,targetLabel):
     return real_outlier,tranTarget
 
 
-
-# def readSourceData(dataName,d,rate,index):
-#     sfPath = 'C:/Matlab_Code/infometric_0.1/Data/' + dataName + '/' + dataName + '_' + str(index) + '_'+str(rate)+'_d=' + str(
-#         d) + '.mat';
-#
-#     data = sio.loadmat(sfPath)
-#     sourceLabel = data['sourceLabel']
-#     targetLabel = data['targetLabel']
-#     sourceFeature = data['sourceFeature']
-#     targetFeature = data['targetFeature']
-#
-#     return sourceLabel,targetLabel,sourceFeature,targetFeature
 
 
 def transform(sourceData,betaValue):
@@ -121,12 +112,6 @@ def initPlusCentroids(dataSet,K):
     centroids = get_centroids(mat(dataSet), K)
     return centroids
 
-# def computeMeanBeta(indexInClust,betaValue):
-#     sum = 0.0
-#     for i in indexInClust:
-#         sum+=betaValue[i]
-#
-#     return sum/size(indexInClust)
 
 def computeSigma(pointsInClust):
     #sigmaValue = 0.0
@@ -140,13 +125,15 @@ def computeSigma(pointsInClust):
     # sigmaValue = sum/arrayLength
     return 1/sqrt(sigmaValue)
 
-def computeRadius(indexInClust,dataSet,centerPoint):
+def computeRadius(indexInClust,dataSet,centerPoint,betaClusValue):
     sum = 0.0
-    max = 0.0
+    maxR = 0.0
     for i in indexInClust:
-        sum+=calcDist(dataSet[i,:],centerPoint)
+        curDist = euclDistance(dataSet[i, :], centerPoint)*betaClusValue
+        maxR = max(maxR,curDist)
 
-    return sum/size(indexInClust)
+    #return sum/size(indexInClust)
+    return maxR
 
 def computeSigmaRadius(indexInClust, dataSet, centerPoint):
     sum = 0.0
@@ -165,88 +152,111 @@ def computeSigmaRadius(indexInClust, dataSet, centerPoint):
 
 def computeBetaRadius(indexInClust, dataSet, centerPoint,betaClusValue):
     sum = 0.0
-    c = 2.0
+    c = 2.5
     distanceList = []
     for i in indexInClust:
-        distanceBeta= euclDistance(dataSet[i, :], centerPoint)*betaClusValue
+        distanceBeta = euclDistance(dataSet[i, :], centerPoint)*betaClusValue
         distanceList.append(distanceBeta)
 
     meanDis = mean(distanceList)
     varDis = var(distanceList)
+
     return meanDis + c * varDis
 
-# def kMeansBeta(dataSet, k, betaValue, distMeas=calcDist, createCent=initCentroids):
-#
-#     m = shape(dataSet)[0]
-#     clusterAssment = mat(zeros((m,2)))#create mat to assign data points
-#                                       #to a centroid, also holds SE of each point
-#     centroids = createCent(dataSet, k)
-#     #centroids,betaCenter = createCent(dataSet, k,betaValue)
-#     clusterChanged = True
-#     radiusCluster = [0] * k
-#     betaCluster = [0] * k
-#     #sigmaCluster = [1] * k
-#     while clusterChanged:
-#         clusterChanged = False
-#         for i in range(m):#for each data point (0,m) assign it to the closest centroid
-#             minDist = inf
-#             minIndex = -1
-#             for j in range(k):
-#                 distJI = calcDist(centroids[j,:],dataSet[i,:])
-#                 #print distJI
-#                 #distBeta = fabs(betaCenter[j] - betaValue[i])
-#                 if betaValue[i]*distJI < minDist: #minimize distance+beta decreasing power(Vec1 - Vec2, 2)
-#                 #if power(distJI, 2) + power(distBeta,2) < minDist:
-#                     minDist = betaValue[i]*distJI
-#                     minIndex = j
-#             if clusterAssment[i,0] != minIndex:
-#                 clusterChanged = True
-#             clusterAssment[i,:] = minIndex,minDist**2
-#
-#
-#         for j in range(k):#recalculate centroids
-#             indexInClust = nonzero(clusterAssment[:, 0].A == j)[0]
-#             #print "The size of cluster"+str(j)+"in EM is:",size(indexInClust)
-#             #betaCenter[j] = computeMeanBeta(indexInClust,betaValue)
-#
-#             ptsInClust = dataSet[indexInClust]#get all the point in this cluster
-#             centroids[j,:] = mean(ptsInClust, axis=0) #assign centroid to mean
-#
-#             betaClusSum = 0.0
-#             count = 0.0
-#             for i in indexInClust:
-#                 betaClusSum+=betaValue[i]
-#                 count+=1
-#             betaCluster[j] = betaClusSum/count
-#
-#             radiusCluster[j] = computeBetaRadius(indexInClust, dataSet, centroids[j, :], betaCluster[j])
-#
-#             #radiusCluster[j] = computeSigmaRadius(indexInClust, dataSet, centroids[j, :])
-#
-#
-#     print "========================================================================================================="
-#
-#     return centroids,clusterAssment,radiusCluster,betaCluster
-#     #return centroids, clusterAssment, radiusCluster
-
-
-
-def kMeansBeta(ori_dataSet, k, start, end, betaValue, distMeas=calcDist, createCent=initCentroids):
-
-    #m = shape(dataSet)[0]
-    m = end - start
+# k-means cluster
+def ori_kmeans(ori_dataSet, k,start,end,souceLabel):
     dataSet = ori_dataSet[start:end]
+    numSamples = dataSet.shape[0]  # 行数
+    clusRealLabel = [0] * k
+    clusterAssment = mat(zeros((numSamples, 2)))  #
+    realIndexSet = {}
+    clusterChanged = True  # 停止循环标志位
+
+    ## step 1: init 初始化k个质点
+    centroids = initCentroids(dataSet, k)
+    radiusCluster = [0] * k
+    while clusterChanged:
+        clusterChanged = False
+        ## for each 行
+        for i in xrange(numSamples):
+            minDist = 100000.0  # 设定一个极大值
+            minIndex = 0
+            ## for each centroid
+            ## step 2: 寻找最接近的质心
+            for j in range(k):
+                distance = euclDistance(centroids[j, :], dataSet[i, :])
+                # 将centroids（k个初始化质心）的j行和dataset（数据全集）的i行 算欧式距离，返回数值型距离
+                if distance < minDist:
+                    # 找距离最近的质点，记录下来。
+                    minDist = distance
+                    minIndex = j
+
+            ## step 3: update its cluster # 跟新这个簇
+            if clusterAssment[i, 0] != minIndex:
+                clusterChanged = True  # clusterAssment 是一个n行2列的矩阵  Assment 评估
+                clusterAssment[i, :] = minIndex, minDist ** 2  # 赋值为 新的质点标号
+
+        ## step 4: update centroids
+        for j in range(k):
+            # 属于j这个质点的所有数值的平均值算出成为新的质点
+            indexInClust = nonzero(clusterAssment[:, 0].A == j)[0]
+            pointsInCluster = dataSet[indexInClust]
+            centroids[j, :] = mean(pointsInCluster, axis=0)
+            # caculate the radius of every cluster
+            radiusCluster[j] = computeSigmaRadius(indexInClust, dataSet, centroids[j, :])
+
+    print 'Congratulations, cluster complete!'
+
+    # assign clusterID to real index
+    realIndexClus = {}
+    index = start
+    for assignItem in clusterAssment[:, 0].tolist():
+        # print assignItem[0]
+        realIndexClus[index] = assignItem[0]
+        index += 1
+    # print "real index length: ",len(realIndexClus)
+    print "========================================================================================================="
+    # assign real label
+    for j in range(k):
+        indexInClust = nonzero(clusterAssment[:, 0].A == j)[0]
+        clusRealLabel[j] = clusLabel(indexInClust, sourceLabel)
+
+    return centroids, realIndexClus, radiusCluster,clusRealLabel
+
+def clusLabel(indexInClust,sourceLabel):
+    labelSet = {}
+    maxPoint = 0.0
+    clusAssignLabel = 0
+    for item in indexInClust:
+        pointLabel = sourceLabel[item]
+        if pointLabel not in labelSet.keys():
+            labelSet[pointLabel] = 0
+
+        labelSet[pointLabel] += 1
+
+    for key in labelSet:
+        if labelSet[key] > maxPoint:
+            maxPoint = labelSet[key]
+            clusAssignLabel = key
+
+    return clusAssignLabel
+
+def kMeansBeta(dataSet, k, betaValue, sourceLabel,distMeas=calcDist, createCent=initCentroids):
+
+    m = shape(dataSet)[0]
+    #m = end - start
+    #dataSet = ori_dataSet[start:end]
     #m = 100000
-    clusterAssment = mat(zeros((m,2)))#create mat to assign data points
-                                      #to a centroid, also holds SE of each point
+    clusterAssment = mat(zeros((m,2)))#create mat to assign data points to a centroid, also holds SE of each point
+    clusterPoints = [[]]*k  # create array to store data points
     centroids = createCent(dataSet, k)
     #centroids,betaCenter = createCent(dataSet, k,betaValue)
     clusterChanged = True
     radiusCluster = [0] * k
     betaCluster = [0] * k
-    realIndexClus = {}
+    clusRealLabel = [0] * k
+
     #sigmaCluster = [1] * k
-    i = start
     while clusterChanged:
         clusterChanged = False
         for i in range(m):#for each data point (0,m) assign it to the closest centroid
@@ -268,10 +278,8 @@ def kMeansBeta(ori_dataSet, k, start, end, betaValue, distMeas=calcDist, createC
 
         for j in range(k):#recalculate centroids
             indexInClust = nonzero(clusterAssment[:, 0].A == j)[0]
-            for index in indexInClust:
-                realIndexClus[index] = j
-
             ptsInClust = dataSet[indexInClust]#get all the point in this cluster
+            clusterPoints[j] = ptsInClust
             centroids[j,:] = mean(ptsInClust, axis=0) #assign centroid to mean
 
             betaClusSum = 0.0
@@ -279,19 +287,22 @@ def kMeansBeta(ori_dataSet, k, start, end, betaValue, distMeas=calcDist, createC
             for i in indexInClust:
                 betaClusSum+=betaValue[i]
                 count+=1
-            betaCluster[j] = betaClusSum/count
+            if(count != 0):
+                betaCluster[j] = betaClusSum/count
+            else:
+                betaCluster[j] = 0.0
 
-            radiusCluster[j] = computeBetaRadius(indexInClust, dataSet, centroids[j, :], betaCluster[j])
-           #radiusCluster[j] = computeSigmaRadius(indexInClust, dataSet, centroids[j, :])
-    betaSet = {}
-    i = start
-    for betaItem in betaValue:
-        betaSet[i] = betaItem
-        i += 1
+            #radiusCluster[j] = computeBetaRadius(indexInClust, dataSet, centroids[j, :], betaCluster[j])
+            radiusCluster[j] = computeRadius(indexInClust, dataSet, centroids[j, :],betaCluster[j])
+
 
     print "========================================================================================================="
+    #assign real label
+    for j in range(k):
+        indexInClust = nonzero(clusterAssment[:, 0].A == j)[0]
+        clusRealLabel[j] = clusLabel(indexInClust, sourceLabel)
 
-    return centroids,realIndexClus,radiusCluster,betaCluster,betaSet
+    return centroids,clusterPoints,radiusCluster,clusRealLabel,betaCluster
     #return centroids, clusterAssment, radiusCluster
 
 
@@ -305,6 +316,29 @@ def Betapredict(dataSet,centroids,k,radiusCluster,betaCluster):
         outlier = True
         for j in range(k):
             distance = calcDist(dataSet[i,:],centroids[j,:])*betaCluster[j]
+            if distance<=radiusCluster[j]:
+                outlier = False
+                break
+        #count+=1;
+
+        if outlier == False:
+            continue
+        else:
+            count+=1
+
+    print "count",count
+    print "m",m
+    print "outlier accuracy: ", count/m
+
+def normalPredict(dataSet,centroids,k,radiusCluster):
+    m = dataSet.shape[0] #number of the dataSet
+    count = 0.0
+    for i in range(m):
+        #everyCluster = 0.0
+        #print "i: ",i
+        outlier = True
+        for j in range(k):
+            distance = calcDist(dataSet[i,:],centroids[j,:])
             #print "redius of center "+str(j)+" is: ",radiusCluster[j]
             #print "distance of unknow label "+str(i)+" to center is: ",distance
             if distance<=radiusCluster[j]:
@@ -321,56 +355,47 @@ def Betapredict(dataSet,centroids,k,radiusCluster,betaCluster):
     print "m",m
     print "outlier accuracy: ", count/m
 
-# def BetacalcPurity(clusterAssment,k,sourceLabel):
-#     purity = []
-#     for j in range(k):
-#         #everyInfo = 0.0
-#         maxPoint = 0.0
-#         minPoint = 0.0
-#         labelSet = {}
-#         indexInClust = nonzero(clusterAssment[:, 0].A == j)[0]
-#
-#         total = len(indexInClust)*1.0
-#         print "In"+str(j)+" th cluster, the total point is",total
-#         for i in indexInClust:
-#             pointLabel = sourceLabel[i]
-#             if pointLabel not in labelSet.keys():
-#                 labelSet[pointLabel] = 0
-#
-#             labelSet[pointLabel]+=1
-#
-#         for key in labelSet:
-#             maxPoint = max(maxPoint,labelSet[key])
-#
-#         print "maxLabel number", maxPoint
-#         purity.append(maxPoint/total)
-#
-#     return purity
 
+def assignLabel(targetPoint,centroids,clusRealLabel):
+    minDisToClus = 1000000000.0000
+    minClusID = 0.0
+
+    k = len(centroids)
+    for j in range(k):
+        curDis = euclDistance(targetPoint, centroids[j, :])
+        if curDis < minDisToClus:
+            minClusID = j
+            minDisToClus = curDis
+
+    predictLabel = clusRealLabel[minClusID]
+    return predictLabel
 
 
 if __name__ == '__main__':
-    gammab = [10.0]
+    gammab = [32.0]
     dataName = "Syndata_002"
     rate = 0.3
-    K = 5
+    K = 8
     index = 1
     novelClassList = []
-    start = 100
-    end = 1800
+    start = 0
+    end = 500
 
-    sourceFeature, targetFeature, sourceLabel, targetLabel = readStreamData(dataName,rate,index)
+    sourceFeature, targetFeature, sourceLabel, targetLabel = readBetaStreamData(dataName,rate,index)
+    targetFeature = targetFeature[start:end * 2]
+    targetLabel = targetLabel[start:end * 2]
     print "source data length: ",len(sourceFeature)
     print "target data length: ",len(targetFeature)
 
-    beta_sourceFeature = sourceFeature[start:end]
-    betaValue = getBeta(beta_sourceFeature, targetFeature, gammab)
+    #beta_sourceFeature = sourceFeature[start:end]
+    betaValue = getBeta(sourceFeature, targetFeature, gammab)
     print "length of beta value: ",len(betaValue)
 
     #tranSourceFeature = transform(sourceFeature,betaValue)
     #print "tranSourceFeature: ",tranSourceFeature
 
-    centroids, realIndexClus, radiusCluster, betaCluster, betaSet = kMeansBeta(mat(sourceFeature), K, start, end, betaValue)
+    centroids, clusterPoints, radiusCluster, clusRealLabel, betaCluster = kMeansBeta(mat(sourceFeature), K, betaValue,sourceLabel)
+    #print "K number: ",len(centroids)
     real_outlier, tranTarget = getNovelClass(targetFeature, targetLabel)
 
     # purity = BetacalcPurity(clusterAssment,K,sourceLabel)
@@ -387,4 +412,38 @@ if __name__ == '__main__':
     print "For novel class: "
     Betapredict(mat(real_outlier), centroids, K, radiusCluster, betaCluster)
 
+    print "========================================================================"
+    all_test = len(targetLabel)
+    right = 0.0
+    for index,point in enumerate(targetFeature):
+        predictLabel = assignLabel(point,centroids,clusRealLabel)
+        if(predictLabel == targetLabel[index]):
+            right+=1
 
+    print "whole predict target point is: ",all_test
+    print "Accuracy: ",right/all_test
+
+
+    # kmeans algorithm
+    kmeans = KMeans(n_clusters=8, random_state=0).fit(sourceFeature)
+    labels = kmeans.labels_
+    clusId2label = clus_Info(labels, 8, sourceLabel)
+    predict_same = kmeans.predict(targetFeature)
+
+    count_label = 0.0
+    predict_tran = np.ones(shape=len(predict_same), dtype=int) * -1
+    for index, predictId in enumerate(predict_same):
+        #print "clusId2label[predictId]: ",clusId2label[predictId]
+        predict_tran[index] = clusId2label[predictId]
+        if predict_tran[index] == targetLabel[index]:
+            count_label+=1
+
+    print "len of same label target: ",len(targetLabel)
+    print count_label/len(targetLabel)
+
+    #svm
+    # svc = svm.SVC(C = 20.0, kernel = 'rbf',gamma = 0.1)
+    # svc.fit(np.array(sourceFeature), sourceLabel, sample_weight=None)
+    # pre = svc.predict(np.array(targetFeature))
+    # acc = float((pre == targetLabel).sum()) / len(targetLabel)
+    # print "accuracy is: ",acc
